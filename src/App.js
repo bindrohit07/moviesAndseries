0935f928fcbed554e0089c0202e3ff7ec7ba6b88
Rohit Kumar Bind <rohitbind9@gmail.com>
2022-08-02 20:21:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import Cards from './Cards';
+
+function App() {
+  return (
+  <>
+<h1 className='heading'> Best hindi movies and series </h1>
+<Cards/>
+<Cards/>
+<Cards/>
+<Cards/>
+<Cards/>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function Cards(){
+    return(
+       <>
+        <div className='cards'>
+        <div className='card'> 
+        <img  className='kgf' src='https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/49d679a2183c1faa41805762e8d1b709b590397aa80af64ca013adbcdfb4c34b._V_SX1440_.jpg' alt='kgf'></img>
+        <h3>KGF Chapter 2</h3>
+        <button className="btn"> <a href='https://www.primevideo.com/detail/amzn1.dv.gti.9f72e5c0-8dce-4d9c-bc54-c7ab9844f654?ref_=dvm_pds_gen_in_kk_s_gt_kgf2_Desktop' >WATCH NOW </a></button>
+        </div>
+         </div>
+      </>
+    )
+} 
+export default Cards;
