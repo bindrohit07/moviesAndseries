35cfef090d4ecb0279ba5a253bb8346aad33cc01
Rohit Kumar Bind <rohitbind9@gmail.com>
2022-10-15 12:41:03 +0530

    App changes

---
+++ src/Api.jsx
@@ -0,0 +1,57 @@
+const Api =[
+    {
+     imgsrc:'https://images-eu.ssl-images-amazon.com/images/S/pv-target-images/49d679a2183c1faa41805762e8d1b709b590397aa80af64ca013adbcdfb4c34b._V_SX1440_.jpg',
+     sname:'KGF Chapter 2',
+     link:'https://www.primevideo.com/detail/amzn1.dv.gti.9f72e5c0-8dce-4d9c-bc54-c7ab9844f654?ref_=dvm_pds_gen_in_kk_s_gt_kgf2_Desktop',
+    
+    },
+    {
+        imgsrc:'https://m.media-amazon.com/images/S/pv-target-images/844421138928023075289343bd51861453acdc82a415968af5b434b1065d642e._UR1920,1080_SX356_FMwebp_.jpg',
+        sname:'PUSHPA: The Rise',
+        link:'https://www.primevideo.com/detail/0QH32UG7NRSFRGZT78NFD6KED7/ref=atv_mv_hom_1_c_2eQwSg_brws_2_2',
+
+       },
+       {
+        sname:'Satyameva Jayate',
+        imgsrc:'https://m.media-amazon.com/images/S/pv-target-images/c0db261b907d0d27fe89c636fdabc453fb3784d4b5f4ddd22490d745a231b618._UR1920,1080_UX400_UY225_.jpg',
+        link:'https://www.primevideo.com/detail/0I5NWIA6SO8DDODDUWB6506BGY/ref=atv_sr_def_c_unkc_2_1_2?sr=1-2&pageTypeIdSource=ASIN&pageTypeId=B07HJH5FB4&qid=1665812470555'
+       },
+       {
+        sname:'Criminal Justice ',
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/3615/753615-h',
+        link:'https://www.hotstar.com/in/tv/criminal-justice/1260004267/once-upon-a-night/1000232940',
+       },
+       {
+        sname:'The Family Man',
+        imgsrc:'https://m.media-amazon.com/images/S/pv-target-images/dc0be6ae04e6eb4ea5fd564250445f38c0fe6243fc1a9d20ed9e93575ecaf561._UR1920,1080_UX400_UY225_.jpg',
+        link:'https://www.primevideo.com/detail/0S3QYI59BAEI5KVLHCKSR91YGD/ref=atv_sr_def_c_unkc_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07X443FLN&qid=1665812898058',
+       },
+       {
+        sname:'Mirzapur',
+        imgsrc:'https://m.media-amazon.com/images/S/pv-target-images/c5e1ffea964b645052c3101cf403868a721da5f34366c515fde9306be8d7e54a._UR1920,1080_UX400_UY225_.jpg',
+        link:'https://www.primevideo.com/detail/0PDOKMV9CRLOMO5EUKNCUJLG4Q/ref=atv_sr_def_c_unkc_1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B07JJ77FD8&qid=1665812941686',
+       },
+       {
+        sname:'Avengers: ENDGAME',
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4469/674469-h',
+        link:'https://www.hotstar.com/in/movies/avengers-endgame/1260013556/watch?bookmarkTime=10161.231571920001',
+       },
+       {
+        sname:'Avengers: INFINITY WAR',
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5615/675615-h',
+        link:'https://www.hotstar.com/in/movies/avengers-infinity-war/1660010677/watch',
+       },
+       {
+        sname:'CIVIL WAR',
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4458/674458-h',
+        link:'https://www.hotstar.com/in/movies/captain-america-civil-war/1260016768/watch',
+       },
+       {
+        sname:'Housefull 4',
+        imgsrc:'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/3668/593668-h',
+        link:'https://www.hotstar.com/in/movies/housefull-4/1260014263/watch',
+       },
+      
+
+    ];
+    export default Api;
--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 //import logo from './logo.svg';
 import './App.css';
-import Cards from './Cards';
-
+import Card from './Cards';
+import Api from './Api';
 function App() {
   return (
   <>
 <h1 className='heading'> Best hindi movies and series </h1>
-<Cards/>
-<Cards/>
-<Cards/>
-<Cards/>
-<Cards/>
-
+<Card
+imgsrc={Api[0].imgsrc}
+sname={Api[0].sname}
+link={Api[0].link}
+/>
+<Card
+imgsrc={Api[1].imgsrc}
+sname={Api[1].sname}
+link={Api[1].link}
+/>
+<Card
+imgsrc={Api[2].imgsrc}
+sname={Api[2].sname}
+link={Api[2].link}
+/>
+<Card
+imgsrc={Api[3].imgsrc}
+sname={Api[3].sname}
+link={Api[3].link}
+/>
+<Card
+imgsrc={Api[4].imgsrc}
+sname={Api[4].sname}
+link={Api[4].link}
+/>
+<Card
+imgsrc={Api[5].imgsrc}
+sname={Api[5].sname}
+link={Api[5].link}
+/>
+<Card
+imgsrc={Api[6].imgsrc}
+sname={Api[6].sname}
+link={Api[6].link}
+/>
+<Card
+imgsrc={Api[7].imgsrc}
+sname={Api[7].sname}
+link={Api[7].link}
+/>
+<Card
+imgsrc={Api[8].imgsrc}
+sname={Api[8].sname}
+link={Api[8].link}
+/>
+<Card
+imgsrc={Api[9].imgsrc}
+sname={Api[9].sname}
+link={Api[9].link}
+/>
+<h3 className='footer'>© 2022 moviesAndseries </h3>
   </>
   );
 }
