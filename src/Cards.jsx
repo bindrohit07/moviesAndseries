2d175d4b80f56f7f498600b11b43ee38ec2734f3
Rohit Kumar Bind <rohitbind9@gmail.com>
2022-10-16 10:00:13 +0530

    App.css changes

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -7,7 +7,7 @@ function Card(props)
      
         <div className="cards">
         <div className='card'> 
-        <img className="kgf" src={props.imgsrc}></img>
+        <img className="kgf" src={props.imgsrc} alt="it is a movie image"></img>
         <h3>{props.sname}</h3>
         <button className="btn"> <a href={props.link} >WATCH NOW </a></button>
         </div>
